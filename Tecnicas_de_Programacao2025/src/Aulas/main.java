78fb772ac7f047126822eb40e7e2589d3fb525d3
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-04-03 09:40:33 -0300

    update

--- Tecnicas_de_Programacao2025/src/Aulas/main.java
+++ Tecnicas_de_Programacao2025/src/Aulas/main.java
@@ -3,7 +3,7 @@ package Aulas;
 public class Main {
 	public static void main(String args[]) {
 		Pessoa p = new Pessoa("Juliano", "123.456.789-00");
-		System.out.println(p.getNome());
-		System.out.println(p.getCPF());
+		System.out.println("Nome: " + p.getNome());
+		System.out.println("CPF: " + p.getCPF());
 	}
 }
