ff83c07045a995713034127f2e576e45c2759776
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-04-08 09:32:51 -0300

    update

--- Tecnicas_de_Programacao2025/BankSys/src/BankSys/CriaConta.java
+++ Tecnicas_de_Programacao2025/BankSys/src/BankSys/CriaConta.java
@@ -4,11 +4,14 @@ public class CriaConta {
 	public static void main(String[] args) {
 		Conta conta;
 		conta = new Conta("1234-5", "Juliano");
-		conta.creditar(1376.98);
-		conta.debitar(287.54);
+		Conta conta1;
+		conta1 = new Conta("1234-6", "Matias");
+		if(conta.nome().equals(conta1.nome())) {
+			System.out.println("NOME IGUAL");
+		}
 		System.out.println("Conta Bancária de " + conta.nome());
 		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-		System.out.println("Número de Conta: " + conta.numero());
+		System.out.println("Número de Conta de " + conta.nome() + ": " + conta.numero());
 		System.out.println("Saldo: " + conta.saldo());
 	}
 }
