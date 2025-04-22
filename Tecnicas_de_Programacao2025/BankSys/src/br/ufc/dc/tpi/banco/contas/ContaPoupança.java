37b535fb21dbfc7f68eb90cf1e8d951ad9c9f3af
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-04-22 08:54:22 -0300

    poupança

---
+++ Tecnicas_de_Programacao2025/BankSys/src/br/ufc/dc/tpi/banco/contas/ContaPoupança.java
@@ -0,0 +1,12 @@
+package br.ufc.dc.tpi.banco.contas;
+
+public class ContaPoupança extends Conta{
+	
+	public ContaPoupança(String numero, String nome) {
+		super(numero, nome);
+	}
+	
+	public void renderJuros(double taxa) {
+		creditar(GetSaldo() * taxa);
+	}
+}
