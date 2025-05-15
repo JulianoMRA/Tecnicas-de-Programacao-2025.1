87e66a8962b8974d1361a1358272c05c568297f0
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-15 18:50:34 -0300

    Implementando a Classe Prescrição

--- Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/prescricao/Prescricao.java
+++ Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/prescricao/Prescricao.java
@@ -1,5 +1,48 @@
 package br.com.pharmasys.model.prescricao;
 
-public class Prescricao {
+import br.com.pharmasys.model.medicamento.*;
+import br.com.pharmasys.model.medico.*;
+import br.com.pharmasys.model.paciente.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Prescricao {
+	private Paciente paciente;
+	private Medico medico;
+	private List<Medicamento> medicamentos;
+	
+	// Construtor
+	public Prescricao(Paciente paciente, Medico medico) {
+        this.paciente = paciente;
+        this.medico = medico;
+        this.medicamentos = new ArrayList<>();
+    }
+	
+	// Getters
+	public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+    
+    public List<Medicamento> getMedicamentos() {
+        return Collections.unmodifiableList(medicamentos);
+    }
+    
+    // Printador da Prescrição (GPT)
+    public String resumoPrescricao() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Paciente: ").append(paciente.getNome()).append(" ").append(paciente.getSobrenome()).append("\n");
+        sb.append("Médico: ").append(medico.getNome()).append(" ").append(medico.getSobrenome()).append("\n");
+        sb.append("Medicamentos prescritos:\n");
+        for (Medicamento med : medicamentos) {
+            sb.append("- ").append(med.getTipo()).append(": ").append(med.getNome()).append("\n");
+            sb.append("  Posologia: ").append(med.descricaoPosologia()).append("\n");
+        }
+        return sb.toString();
+    }
 }
