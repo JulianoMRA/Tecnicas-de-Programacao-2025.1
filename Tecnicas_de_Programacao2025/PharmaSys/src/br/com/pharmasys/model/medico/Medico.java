72dba6a83c6581a2c48b7f0cf1ac4eb301b624eb
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-15 18:30:07 -0300

    Implementando a Classe Medico

--- Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medico/Medico.java
+++ Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medico/Medico.java
@@ -1,5 +1,49 @@
 package br.com.pharmasys.model.medico;
 
 public class Medico {
+	private String nome;
+	private String sobrenome;
+	private String crm;
+	private String especialidade;
+	
+	// Construtor
+	public Medico(String nome, String sobrenome, String crm, String especialidade) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.crm = crm;
+		this.especialidade = especialidade;
+	}
+	
+	// Getters e Setters
+	public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
 }
--- Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/paciente/Paciente.java
+++ Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/paciente/Paciente.java
@@ -1,5 +1,59 @@
 package br.com.pharmasys.model.paciente;
 
 public class Paciente {
-
+	private String nome;
+	private String sobrenome;
+	private String cpf;
+	private String plano;
+	private int idade;
+	
+	// Construtor
+	public Paciente(String nome, String sobrenome, String cpf, String plano, int idade) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.cpf = cpf;
+		this.plano = plano;
+		this.idade = idade;
+	}
+	
+	// Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getPlano() {
+		return plano;
+	}
+	
+	public void setPlano(String plano) {
+		this.plano = plano;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
