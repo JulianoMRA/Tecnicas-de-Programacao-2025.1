688b53c6e8872c9e229056b005cbf2ee67b180d2
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-15 18:41:41 -0300

    Implementando Comprimido

--- Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medicamento/Comprimido.java
+++ Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medicamento/Comprimido.java
@@ -1,5 +1,18 @@
 package br.com.pharmasys.model.medicamento;
 
-public class Comprimido {
+public class Comprimido extends Medicamento{
+	// Construtor
+	public Comprimido(String nome, double doseDiaria, int quantidade, String dosagem) {
+		super(nome, doseDiaria, quantidade, dosagem);
+	}
 
+	public String getTipo() {
+		return "Comprimido";
+	}
+
+	public String descricaoPosologia() {
+		return String.format("Tomar %.0f comprimidos por dia, totalizando %d comprimidos de %s cada.",
+                doseDiaria, quantidade, dosagem);
+	}
+	
 }
