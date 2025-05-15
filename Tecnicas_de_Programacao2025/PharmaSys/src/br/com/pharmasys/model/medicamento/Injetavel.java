935b929d490682a82335756df75a4288888e992c
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-15 18:43:37 -0300

    Implementando Injetável

--- Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medicamento/Injetavel.java
+++ Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medicamento/Injetavel.java
@@ -1,5 +1,17 @@
 package br.com.pharmasys.model.medicamento;
 
-public class Injetavel {
+public class Injetavel extends Medicamento {
+	// Construtor
+    public Injetavel(String nome, double doseDiaria, int quantidade, String dosagem) {
+        super(nome, doseDiaria, quantidade, dosagem);
+    }
 
+    public String getTipo() {
+        return "Injetável";
+    }
+
+    public String descricaoPosologia() {
+        return String.format("Aplicar %.0f ampolas por dia, totalizando %d ampolas de %s cada.",
+                doseDiaria, quantidade, dosagem);
+    }
 }
