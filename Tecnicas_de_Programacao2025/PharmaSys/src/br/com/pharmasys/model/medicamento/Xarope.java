0af5b03d684d298098046c4b3b699f40b0672fc7
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-15 18:42:29 -0300

    Implementando Xarope

--- Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medicamento/Xarope.java
+++ Tecnicas_de_Programacao2025/PharmaSys/src/br/com/pharmasys/model/medicamento/Xarope.java
@@ -1,5 +1,17 @@
 package br.com.pharmasys.model.medicamento;
 
-public class Xarope {
+public class Xarope extends Medicamento {
+	// Construtor
+    public Xarope(String nome, double doseDiaria, int quantidade, String dosagem) {
+        super(nome, doseDiaria, quantidade, dosagem);
+    }
 
+    public String getTipo() {
+        return "Xarope";
+    }
+
+    public String descricaoPosologia() {
+        return String.format("Tomar %.1f ml por dia, totalizando %d ml de %s.",
+                doseDiaria, quantidade, dosagem);
+    }
 }
